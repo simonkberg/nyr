0738e21d09bb2e5b9fc82f78461763772728e33b
Simon Kjellberg <simon.kjellberg@gmail.com>
2017-01-31 10:04:42 +0100

    fix: use `npm_execpath` for bin detection (#1)
    Seems to be more reliable than `_`.
    
    BREAKING CHANGE:
    Possible this doesn't work correctly on some environments.
    Please open an issue in that case.

--- test/yarn.js
+++ test/yarn.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const { bin, args, cwd, isYarn, isNPM } = require('../index')
 
-assert(bin.endsWith('yarn'), `'bin' is '${bin}'`)
+assert(bin.endsWith('yarn.js'), `'bin' is '${bin}'`)
 assert(args.indexOf('run') === 0, `'args' is '${args}'`)
 assert(cwd === process.cwd(), `'cwd' is '${cwd}'`)
 assert(isYarn === true, `'isYarn' is '${isYarn}'`)
