14423d6a76fce96f6f4370dbd44a7171807cbd20
Simon Kjellberg <simon.kjellberg@gmail.com>
2017-03-05 17:20:58 +0100

    feat(compatibility): Refactor for compatibility with node v4 (#4)

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const {
-  env: { npm_execpath: bin, DEBUG },
-  argv,
-  cwd,
-} = process
+'use strict'
 
-const args = argv.slice(2)
+const bin = process.env.npm_execpath
+const args = process.argv.slice(2)
 
 if (args[0] !== 'run') {
   args.unshift('run')
@@ -13,13 +10,13 @@ if (args[0] !== 'run') {
 module.exports = {
   bin,
   args,
-  cwd: cwd(),
+  cwd: process.cwd(),
   isYarn: bin.endsWith('yarn.js'),
   isNPM: bin.endsWith('npm-cli.js'),
 }
 
-if (DEBUG === 'nyr') {
-  const { inspect } = require('util')
+if (process.env.DEBUG === 'nyr') {
+  const inspect = require('util').inspect
   const colors = process.stdout.isTTY
   console.log(
     '-- nyr DEBUG START --\n%s\n-- nyr DEBUG END --',
